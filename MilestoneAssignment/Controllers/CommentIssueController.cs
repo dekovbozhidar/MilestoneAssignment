45015612d3712b230565a7061fafe82d57f50f0f
Bozhidar Dekov <dekovbozhidar@gmail.com>
2022-07-25 16:10:17 +0300

    update token

--- MilestoneAssignment/Controllers/CommentIssueController.cs
+++ MilestoneAssignment/Controllers/CommentIssueController.cs
@@ -14,21 +14,22 @@ public class GitHookController : Controller
         {
             var txt = await reader.ReadToEndAsync();
             JObject json = JObject.Parse(txt);
-            if (json["action"].ToString().Equals("opened"))
-            {
-                HttpClient client = new HttpClient();
-                var token = "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";
+           
+            HttpClient client = new HttpClient();
+            var token = "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";
 
-                client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
-                var postData = JToken.Parse("{\"body\": \"Thanks for submitting this issue. Our Team will get back to you as soon as possible!\"}");
+            client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
+            var postData = JToken.Parse("{\"body\": \"Thanks for submitting this issue. Our Team will get back to you as soon as possible!\"}");
 
-                var stringContent = new StringContent(postData.ToString(), UnicodeEncoding.UTF8, "application/json");
-                client.PostAsync(json["issue"]["url"].ToString(), stringContent);
+            var stringContent = new StringContent(postData.ToString(), UnicodeEncoding.UTF8, "application/json");
+            client.PostAsync(json["issue"]["url"].ToString(), stringContent);
+            var resposne = await client.PostAsync(json["issue"]["url"].ToString(), stringContent);
+            resposne.EnsureSuccessStatusCode();
+            string content = await resposne.Content.ReadAsStringAsync();
 
 
-            }
 
         }
         return Ok();
